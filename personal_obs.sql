b3e37b617d4400f64fea70c7b97d9b6ac79b5b5d
Pronka <zaxxonlink@gmail.com>
2023-11-23 04:03:24 +0300

    UPD

--- personal_obs.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `personal_obs_loot` (
-  `account_id` int NOT NULL,
-  `item_id` int NOT NULL,
-  `item_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`account_id`,`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- used_promo.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `character_used_promocodes` (
-  `character_id` int NOT NULL,
-  `promo_code` varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`character_id`,`promo_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
